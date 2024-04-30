4db57d33ad10d006bf59e69a93c50713634fd64b
Jesse <wjesse12@gmail.com>
2024-04-30 01:44:11 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const utils = require("./utils");
+const transposition = require('./TranspositionCipher');
+
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    
+let cipher = "EFNTEASPHOIFGOEHTIDRRDIENEFTSEYANHNEEATITTLRHNNTTNFEYGTHEEVLTNES";
+
+utils.FrequencyAnalysis(cipher);
